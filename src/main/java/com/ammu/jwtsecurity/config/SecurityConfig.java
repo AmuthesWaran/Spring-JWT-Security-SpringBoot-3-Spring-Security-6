57e0bad815ff90028e1f0fcc919e9085ec74c20b
Amutheswaran <amutheswaran@outlook.com>
2023-04-10 21:50:23 +0530

    initial commit3

--- src/main/java/com/ammu/jwtsecurity/config/SecurityConfig.java
+++ src/main/java/com/ammu/jwtsecurity/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.ammu.jwtsecurity.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,22 +10,22 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import jakarta.servlet.Filter;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	
-	private AuthenticationProvider authenticationProvider;
+
+	@Autowired
 	private JWTFilter jwtFilter;
+	private AuthenticationProvider authenticationProvider;
 	
+	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 		
 		http
 			.csrf()
 			.disable()
 			.authorizeHttpRequests()
-			.requestMatchers("/signup")
+			.requestMatchers("/login")
 			.permitAll()
 			.anyRequest()
 			.authenticated()
--- src/main/java/com/ammu/jwtsecurity/controller/AuthController.java
+++ src/main/java/com/ammu/jwtsecurity/controller/AuthController.java
@@ -1,5 +1,11 @@
 package com.ammu.jwtsecurity.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,9 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ammu.jwtsecurity.model.JWTRequest;
 import com.ammu.jwtsecurity.model.JWTResponse;
+import com.ammu.jwtsecurity.service.AuthUserDetailsService;
+import com.ammu.jwtsecurity.service.JWTUtility;
 
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
 @RestController
 public class AuthController {
+	
+	
+	AuthenticationManager manager;
+	
+	@Autowired
+	JWTUtility jwtUtility;
+	
+	@Autowired
+	AuthUserDetailsService authUserDetailsService;
 
 	@GetMapping("/secured")
 	public String securedEndpoint() {
@@ -17,8 +38,21 @@ public class AuthController {
 	}
 	
 	@PostMapping("/login")
-	public JWTResponse login(@RequestBody JWTRequest jwtRequest) {
-		return null;
+	public JWTResponse login(@RequestBody JWTRequest request) throws Exception {
+		System.out.println("HI there");
+		try {
+			
+			manager.authenticate(
+					new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())	
+					);	
+		}
+		catch( BadCredentialsException e ){
+			throw new Exception("Wrong_Username_or_Password");
+		}		
+		UserDetails userdetail = authUserDetailsService.loadUserByUsername(request.getUsername());		
+		String generateToken = jwtUtility.generateToken(userdetail);
+		return new JWTResponse(generateToken);
+		
 	}
 	
 	
